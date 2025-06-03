1148762cfd6e29d4f7ce9a48b37466572f07f408
Oleksandr <rachok010291@gmail.com>
2025-06-03 21:39:29 +0100

    update files

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-export default {
-  define: {
-    global: {},
-  },
-};
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/goit-js-hw-09/', // назва твого репозиторію на GitHub
+});
